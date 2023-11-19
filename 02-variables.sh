f15aab104c1be827f3405bd867ee3a690b366acc
ManojKumarParuchuri8 <manojkumar.paruchuri532@gmail.com>
2023-11-19 19:24:53 +0530

    solved errors

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,4 +4,4 @@ Number3=$3
 
 echo "Addition of three numbers are $((Number1+Number2+Number3))"
 echo "Subtraction of three numbers are $((Number1-Number2-Number3))"
-echo "Multiplication of three numbers are $((Number1*Number2*Number3))
+echo "Multiplication of three numbers are $((Number1*Number2*Number3))"
