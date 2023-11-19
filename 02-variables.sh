700ce40f00c6a60f7616020b9c6ad6cf092fd77e
ManojKumarParuchuri8 <manojkumar.paruchuri532@gmail.com>
2023-11-19 18:44:44 +0530

    Third commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 Person1=Manoj 
 Person2=Jeevan 
-echo "$Person1"
-echo "$Person2"
+Person3=Buchi 
+echo "$Person1 is a software engineer"
+echo "$Person2 is a masters student"
+echo "$Person3 is a software developer"
