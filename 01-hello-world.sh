76e9f2c1c0218a770c798f16794c24abfed15657
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-03 11:06:42 +0530

    Made some changes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 
 # in shell script this is comment 
-echo "Hello, manoj you are learning Shell script all the best"
-echo "This is your first shell program"
-echo "Hello world"
-echo "New Line deleted"
-echo "New Line added"
+# echo "Hello, manoj you are learning Shell script all the best"
+# echo "This is your first shell program"
+# echo "Hello world"
+# echo "New Line deleted"
+# echo "New Line added"
+a=10
+b=20 
+echo $a+$b 
