37c73db81f25e3959b15aeb266238e30d25ea99b
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-03 10:12:45 +0530

    Shell scripts added

---
+++ 01-hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+# in shell script this is comment 
+echo "Hello, manoj you are learning Shell script all the best"
+echo "This is your first shell program"
+echo "Hello world"
+echo "New Line added"
---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+name="Manoj Kumar"
+profession="Devops Engineer"
+company="Phenom"
+
+echo "My name is $name"
+echo "I am working as a $profession"
+echo "My company name is $company"
