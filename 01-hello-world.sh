c6aa7960cae8e5df5cce31971eceb1cdb61adbfe
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-03 11:12:29 +0530

    Made some changes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-in shell script this is comment 
+#in shell script this is comment 
 echo "Hello, manoj you are learning Shell script all the best"
 echo "This is your first shell program"
 echo "Hello world"
