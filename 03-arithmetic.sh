6da5ea4f86c031c150559694a2e849400796f19a
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-03 18:37:50 +0530

    new files

--- 03-arithmetic.sh
+++ 03-arithmetic.sh
@@ -3,5 +3,9 @@ a=$1
 b=$2 
 add=$(($a+$b))
 multiply=$(($a*$b))
+subtraction=$(($a-$b))
+division=$(($a/$b))
 echo $add
 echo $multiply 
+echo $subtraction 
+echo $division 
---
+++ 07-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+names=("Manoj" "Jeevan" "Buchi")
+echo ${names[0]}
+echo ${names[1]}
+echo ${names[2]}
+echo ${names[@]}
