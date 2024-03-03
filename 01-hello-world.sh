0009bb10a0d703af06194b2509926c3f8b5312cb
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-03 11:10:46 +0530

    Made some changes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,12 +1,8 @@
 #!/bin/bash 
 
-# in shell script this is comment 
-# echo "Hello, manoj you are learning Shell script all the best"
-# echo "This is your first shell program"
-# echo "Hello world"
-# echo "New Line deleted"
-# echo "New Line added"
-a=10
-b=20 
-add=$(($a+$b))
-echo $add 
+in shell script this is comment 
+echo "Hello, manoj you are learning Shell script all the best"
+echo "This is your first shell program"
+echo "Hello world"
+echo "New Line deleted"
+echo "New Line added"
