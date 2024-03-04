803d830929404585424f253fa6a3c6cbd89ea37e
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-04 11:27:26 +0530

    made some modification in script

--- 08-if-elif.sh
+++ 08-if-elif.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 a=10 
 b=20 
-if [$a==$b]
+if [ $a==$b ]
 then 
-    echo "$a is equal to $b"
-elif [$a -gt $b]
+    echo "$a is not equal to $b"
+elif [ $a -gt $b ]
 then 
     echo "$a is greater than $b"
-elif [$a -lt $b]
+elif [ $a -lt $b ]
 then 
     echo "$a is lesser than $b"
 else 
