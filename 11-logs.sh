a0910b2187ce9bdf5f6773623a777ea24bc5834e
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-05 22:52:18 +0530

    made some changes

--- 11-logs.sh
+++ 11-logs.sh
@@ -9,10 +9,10 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
     then 
-        echo -e "ERROR:: $2 is failed   $R manoj $N"
+        echo -e "ERROR:: $2 is failed"
         exit 1 
     else 
-        echo "$2 is  SUCCESS  $G manoj $N"
+        echo "$2 is  SUCCESS"
     fi 
 }
 ID=$(id -u) 
@@ -22,7 +22,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "Script started executing at $TIMESTAMP" &>> $LOGFILE 
  if [ $ID -ne 0 ] 
  then 
-    echo -e " ERROR:: Please run this script with root $R access $N "
+    echo -e " ERROR:: Please run this script with root access"
     exit 1 #you can give other than 0
 else 
     echo "you are root user"
