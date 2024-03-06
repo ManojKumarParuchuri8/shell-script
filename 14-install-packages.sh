6765a31b548abda7972fdb41ed2d9951c3ad479c
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-06 11:24:37 +0530

    Install packages through dynamically

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,1 +1,13 @@
 #!/bin/bash 
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ] 
+ then 
+    echo -e " ERROR:: Please run this script with root access"
+    exit 1 #you can give other than 0
+else 
+    echo "you are root user"
+fi 
+
+echo "All arguments Passed: $@"
