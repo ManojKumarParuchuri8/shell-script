7159872d20106137e74627dc42aa674221b60e07
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-06 12:35:49 +0530

    Script updated with all features

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -5,6 +5,21 @@ ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE 
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is Failed"
+    else 
+        echo "$2 is SUCCESS"
+    fi 
+}
 
 if [ $ID -ne 0 ] 
  then 
@@ -14,4 +29,16 @@ else
     echo "you are root user"
 fi 
 
-echo "All arguments Passed: $@" 
+# echo "All arguments Passed: $@" 
+
+for package in $@ 
+do
+    yum list installed $package &>> $LOGFILE 
+    if [ $> -ne 0 ]
+    then 
+        yum install $package -y &>> $LOGFILE  
+        VALIDATE $? "Installion of $package"
+    else 
+        echo "$package is already installed ... Skipping it"
+    fi 
+done
