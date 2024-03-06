72674d20669247007f98aa304dc6fe50b223d9bc
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-06 12:53:24 +0530

    made some changes

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -34,7 +34,7 @@ fi
 for package in $@ 
 do
     yum list installed $package &>> $LOGFILE 
-    if [ $> -ne 0 ]
+    if [ $? -ne 0 ]
     then 
         yum install $package -y &>> $LOGFILE  
         VALIDATE $? "Installion of $package"
