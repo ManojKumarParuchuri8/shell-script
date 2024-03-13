baf9f25baefcd36c854409bf964a43eaba5faeea
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-13 13:08:45 +0530

    added practice files

--- 20-practice-disk-usage.sh
+++ 20-practice-disk-usage.sh
@@ -15,7 +15,7 @@ do
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then 
-        message+="High disk Usage on $partition: $usage\n"
+        message+="High disk Usage on $partition: $usage<br>"
     fi 
 done <<< $DISK_USAGE
 
