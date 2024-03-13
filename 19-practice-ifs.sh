03f20298761fbe6330724d32bc005cf99adcbb0d
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-13 10:01:35 +0530

    Adding practice files

---
+++ 19-practice-ifs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+file="/etc/passwd"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file ]
+then 
+    echo -e "$R $file does not exist $N"
+fi 
+
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
+do 
+    echo "Username is $username"
+    echo "UserFullName is $user_fullname"
+    echo "shell_path is $shell_path"
+done < $file 
+
