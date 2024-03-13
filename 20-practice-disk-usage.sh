f225b6c53ca4eb42fd0ccef7cc9e2a87e541b186
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-13 12:07:53 +0530

    added practice files

---
+++ 20-practice-disk-usage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+DISK_USAGE=$(df -hT| grep -vE 'tmp|File')
+DISK_THRESHOLD=1 
+message=""
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+while IFS= read line
+do 
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then 
+        message+="High disk Usage on $partition: $usage\n"
+    fi 
+
+done <<< $DISK_USAGE
+
