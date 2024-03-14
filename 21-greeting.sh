d04b0215873b7a5cd790a2dea37ad057bd82491a
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-14 13:45:53 +0530

    added optargs

---
+++ 21-greeting.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+NAME=""
+WISHES=""
+
+USAGE()
+{
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. ex, Good Morning"
+    echo " -h, Display Help and exit"
+}
+
+while getopts ":n:w:h:"  opt; do  #waht we enter it comes to opt
+    case $opt in 
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
+        h|*) USAGE; exit;;
+    esac 
+done 
