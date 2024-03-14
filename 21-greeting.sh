5b9696ef30c22da72d1e0e15d3efbf1cea1d7649
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-14 14:25:41 +0530

    added optargs

--- 21-greeting.sh
+++ 21-greeting.sh
@@ -16,6 +16,18 @@ while getopts ":n:w:h:"  opt; do  #what we enter it comes to opt
     case $opt in 
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-        h|*) USAGE; exit;;
+        \?) echo "Invalid Option -$OPTARG" >&2; USAGE; exit 1;;  #invalid 
+        :) USAGE; exit;;
+        h) USAGE; exit;;
     esac 
 done 
+
+if [ -z $NAME ] || [ -z $WISHES ]   # -z means empty
+then 
+    echo "ERROR:: Both -n and -w are mandatory options."
+    USAGE 
+    exit 1 
+fi 
+
+echo "Hello $NAME $WISHES I have been learning shell script."
+
